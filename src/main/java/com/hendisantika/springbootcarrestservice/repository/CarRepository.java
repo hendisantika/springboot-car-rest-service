dd9d8746a841112bfccfe92c5cf91d095c19cbed
Hendi Santika <hendisantika@gmail.com>
2020-02-21 17:44:33 +0700

    #6 - Add many function in Car Repository

--- src/main/java/com/hendisantika/springbootcarrestservice/repository/CarRepository.java
+++ src/main/java/com/hendisantika/springbootcarrestservice/repository/CarRepository.java
@@ -2,6 +2,9 @@ package com.hendisantika.springbootcarrestservice.repository;
 
 import com.hendisantika.springbootcarrestservice.domain.Car;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -13,4 +16,14 @@ import org.springframework.data.repository.CrudRepository;
  * Time: 17.43
  */
 public interface CarRepository extends CrudRepository<Car, Long> {
+    List<Car> findByBrand(@Param("brand") String brand);
+
+    List<Car> findByModel(@Param("model") String model);
+
+    List<Car> findByFuel(@Param("fuel") String fuel);
+
+    List<Car> findByColor(@Param("color") String color);
+
+    List<Car> findByYear(@Param("year") int year);
+
 }
