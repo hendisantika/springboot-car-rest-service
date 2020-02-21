cae58f56f73da5e938ed99a9414339e8d66d595f
Hendi Santika <hendisantika@gmail.com>
2020-02-21 17:49:18 +0700

    #10 - Add getCarsById API in CarController

--- src/main/java/com/hendisantika/springbootcarrestservice/controller/CarController.java
+++ src/main/java/com/hendisantika/springbootcarrestservice/controller/CarController.java
@@ -4,8 +4,11 @@ import com.hendisantika.springbootcarrestservice.domain.Car;
 import com.hendisantika.springbootcarrestservice.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,6 +28,11 @@ public class CarController {
     @GetMapping
     Iterable<Car> getCars() {
         return carRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    Optional<Car> getCarById(@PathVariable("id") String id) {
+        return carRepository.findById(Long.parseLong(id));
     }
 
 }
