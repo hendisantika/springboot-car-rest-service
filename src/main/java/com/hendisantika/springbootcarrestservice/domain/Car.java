c519f6219a56fe428499309b3d5774bd62f1d410
Hendi Santika <hendisantika@gmail.com>
2020-02-22 14:02:14 +0700

    #17 - Change Primay Key to use Sequence

--- src/main/java/com/hendisantika/springbootcarrestservice/domain/Car.java
+++ src/main/java/com/hendisantika/springbootcarrestservice/domain/Car.java
@@ -17,7 +17,7 @@ import javax.persistence.Id;
 @Entity
 public class Car {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private long id;
     private String brand;
     private String model;
